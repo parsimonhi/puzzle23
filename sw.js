3da0650a6527d9c40c2583b501f6874ff44a04f5
parsimonhi <parsimonhi@gmail.com>
2025-11-28 00:32:23 +0100

    Minor updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // when a resource is modified, then change the version to force a cache update
 // execute Date.now() in the console to generate the number
-const VERSION="v1764272318500";
+const VERSION="v1764285910888";
 console.log(VERSION);
 // the name of the cache
 const CACHE_NAME=`puzzle23-${VERSION}`;
